e4944f40e34840fd4a3415c880d2c45675a1a482
chase-abbott <cabbott93@gmail.com>
2021-05-03 13:34:42 -0700

    all tests passing

--- __tests__/creatures.test.js
+++ __tests__/creatures.test.js
@@ -149,4 +149,21 @@ test('GET /api/creatures', async () => {
   expect(response.body).toEqual(expected);
   
 });
+
+test('GET /api/creatures/1', async () => {
+  const expected =  {
+    url:
+              'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
+    title: 'UniWhal',
+    description: 'A unicorn and a narwhal nuzzling their horns',
+    keyword: 'narwhal',
+    horns: 1
+  };
+  
+  const response = await request.get('/api/creatures/:id');
+  
+  expect(response.status).toBe(200); // 200 = OK
+  expect(response.body).toEqual(expected);
+  
+});
   
